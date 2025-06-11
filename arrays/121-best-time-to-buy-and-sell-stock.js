2d83f4efbec36e417793c770b13efa802eb62e37
Durgesh Pandey <durgeskp@gmail.com>
2025-06-11 21:27:42 +0530

    update 121-best-time-to-buy-and-sell-stock.js

--- arrays/121-best-time-to-buy-and-sell-stock.js
+++ arrays/121-best-time-to-buy-and-sell-stock.js
@@ -7,9 +7,7 @@ function maxProfit(prices) {
     for(let i = 0; i < prices.length - 1; i++) {
         for(let j = i + 1; j < prices.length; j++) {
             let profit = prices[j] - prices[i];
-            if (profit > maxProfit) {
-                maxProfit = profit;
-            }
+            maxProfit = Math.max(maxProfit, profit);
         }
     }
     return maxProfit;
